7d75e8e01d5d84dfa65dfaafb2e7701c4cf8a46f
matthewgisonno <matthew@gisonno.com>
2021-10-14 22:59:10 -0700

    changed responsive behavior to stack the flex items into 1 col for small devices

--- components/printdoctor.js
+++ components/printdoctor.js
@@ -5,20 +5,20 @@
 function PrintDoctor(props) {
     return (
         <div className="doctor-listing">
-            <div className="flex mt-10">
-                <div className="flex-none w-24 mr-5">
+            <div className="md:flex lg:flex mt-10">
+                <div className="flex-none w-24 mx-auto sm:mx-auto md:mr-5 lg:mr-5">
                     <img src={props.image ? props.image : props.defaultavatar} alt="Dr Avatar Picture" className="w-full" />
                 </div>
-                <div className="flex-grow">
-                    <h3 className="text-blue-150 text-lg"><a href={props.url}>{props.fullName}</a></h3>
+                <div className="flex-grow text-center sm:text-center md:text-left lg:text-left">
+                    <h3 className="text-blue-150 text-lg mt-2 sm:mt-2 md:mt-0 lg:mt-0"><a href={props.url}>{props.fullName}</a></h3>
                     {props.specialties.map((prop, key) => (
                         <h4 className="text-lg" key={key}>
                             <div>{prop}</div>
                         </h4>
                     ))}
                 </div>
-                <div className="flex-none w-40 sm:w-40 md:w-56 lg:w-56">
-                    <div className="flex flex-col space-y-4">
+                <div className="flex-none w-full sm:w-full md:w-56 lg:w-56 mt-2 sm:mt-2 md:mt-0 lg:mt-0">
+                    <div className="flex flex-col space-y-4 text-center sm:text-center md:text-left lg:text-left">
                         {props.locations.map((prop, key) => (
                             <div className="doctor-locations-block" key={key}>
                                 <a href={prop.url} className="text-base text-blue-150">{prop.name}</a>
